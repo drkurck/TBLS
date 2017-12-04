89bc32fb7b9c9b0abf6e3f68376f2b72f39d3241
Maxence Vannier <maxence.vannier@gmail.com>
2017-12-04 17:50:25 +0100

    state class ok and situation done

--- src/com/company/State.java
+++ src/com/company/State.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class State {
+    private String name;
+    private float health;
+    private float socialisation;
+    private float maturity;
+
+    public State(String name, float health, float socialisation, float maturity) {
+        this.name = name;
+        this.health = health;
+        this.socialisation = socialisation;
+        this.maturity = maturity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+    public float getSocialisation() {
+        return socialisation;
+    }
+
+    public void setSocialisation(float socialisation) {
+        this.socialisation = socialisation;
+    }
+
+    public float getMaturity() {
+        return maturity;
+    }
+
+    public void setMaturity(float maturity) {
+        this.maturity = maturity;
+    }
 }
--- src/com/company/fileHandler.java
+++ src/com/company/fileHandler.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class fileHandler {
     private String statePath;
@@ -17,14 +19,29 @@ public class fileHandler {
         this.outPath = outPath;
     }
 
+    /**
+     * Reading the file to save all history state
+     */
     public void readFile() {
         Path file = Paths.get(this.statePath);
         try (InputStream in = Files.newInputStream(file);
              BufferedReader reader =
                      new BufferedReader(new InputStreamReader(in))) {
             String line = null;
+            // read each line
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                //System.out.println(line);
+                StringTokenizer tokens = new StringTokenizer(line," ");
+                String[] splitedLine = new String[tokens.countTokens()];
+                int index = 0;
+                //split by space in array
+                while (tokens.hasMoreTokens()) {
+                    splitedLine[index] = tokens.nextToken();
+                    ++index;
+                }
+                State current = new State(splitedLine[0], Float.parseFloat(splitedLine[1]),  Float.parseFloat(splitedLine[2]), Float.parseFloat(splitedLine[3]));
+                State previous = new State(splitedLine[4], Float.parseFloat(splitedLine[5]),  Float.parseFloat(splitedLine[6]), Float.parseFloat(splitedLine[7]));
+                System.out.println(Arrays.toString(splitedLine));
             }
         } catch (IOException x) {
             System.err.println(x);
